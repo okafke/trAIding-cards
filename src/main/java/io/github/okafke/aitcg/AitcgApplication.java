93227fc1f3c5f5aad046268df39fdd9df37ddf6a
0kafke <oliver.kafke@studium.uni-hamburg.de>
2023-11-24 17:27:15 +0100

    Initial Commit

---
+++ src/main/java/io/github/okafke/aitcg/AitcgApplication.java
@@ -0,0 +1,13 @@
+package io.github.okafke.aitcg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AitcgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AitcgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/okafke/aitcg/AitcgApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.okafke.aitcg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AitcgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
