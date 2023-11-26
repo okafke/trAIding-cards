0c398beba656e642d30240d460b7b011a2147be6
okfk <65917827+okafke@users.noreply.github.com>
2023-11-26 20:33:03 +0100

    Progress on drawing Text on images

--- src/main/java/io/github/okafke/aitcg/llm/Prompts.java
+++ src/main/java/io/github/okafke/aitcg/llm/Prompts.java
@@ -8,6 +8,13 @@ public class Prompts {
      * Ensures that the model we use does not say things like: "Certainly! To do "thing you instructed me to do" (basically just our prompt) ...".
      */
     public static final String ONLY_OUTPUT = "Only output your answer, do not repeat my prompt, " +
-            "do not explain your thought-process and do not begin by saying things like \"Certainly!\" or \"Sure!\".";
+            "do not explain your thought-process and do not begin by saying things like \"Certainly!\" or \"Sure!\". ";
+
+    public static final String RANDOM_AUTHOR = "Create a story in the style of a random author (dont tell me which), about this creature. ";
+    public static final String NAME = "Given this prompt, give this creature a cute name in 1-5 words."; // TODO: attributes for names
+
+    public static String limitSentences(int n) {
+        return "Keep your answer in " + n + " sentences. ";
+    }
 
 }
--- src/test/java/io/github/okafke/aitcg/card/CardServiceTest.java
+++ src/test/java/io/github/okafke/aitcg/card/CardServiceTest.java
@@ -8,11 +8,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.Resource;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import java.io.FileOutputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Slf4j
 @SpringBootTest
 public class CardServiceTest {
+    public static final String FRIDGE_TEXT = """
+    In the desolate wasteland, where only shadows whispered of a forgotten world, there emerged a surreal manifestation of man's folly.
+    The flaming refrigerator creature, a grotesque fusion of nature and unnatural, prowled with an insatiable hunger, its fiery eyes reflecting the remnants of a civilization lost.
+    Its presence sent tremors through the land, a chilling reminder of the burning desires and destructive appetites that had led to its monstrous creation.
+    """;
+
     @Autowired
     private CardService cardService;
 
@@ -26,9 +34,46 @@ public class CardServiceTest {
     @SneakyThrows
     void testCardService() {
         byte[] image = fieryFridgeMonsterImage.getContentAsByteArray();
-        AiTCGCard card = new AiTCGCard("Fiery Fridge Monster", "A fiery fridge monster!", image);
-        byte[] output = cardService.createCard(card);
-        assertArrayEquals(fieryFridgeMonsterCard.getContentAsByteArray(), output);
+        // TODO: make this actually test something, once we have attributes and stuff down!
+        AiTCGCard cardWithShortTitle = new AiTCGCard("Fiery Hungry Fridge", FRIDGE_TEXT, image);
+        AiTCGCard cardWithLongTitle = new AiTCGCard("Fiery Fridge Monster, the Terrible!", FRIDGE_TEXT, image);
+        AiTCGCard cardWithVeryLongTitle = new AiTCGCard("Fiery Fridge Monster, the Terrible Super Fridge with ultra powers the movie!", FRIDGE_TEXT, image);
+
+        byte[] cardWithShortTitleBytes = cardService.createCard(cardWithShortTitle);
+        try (FileOutputStream outputStream = new FileOutputStream("fiery_fridge_monster_card.png")) {
+            outputStream.write(cardWithShortTitleBytes);
+        }
+
+        byte[] cardWithLongTitleBytes = cardService.createCard(cardWithLongTitle);
+        try (FileOutputStream outputStream = new FileOutputStream("fiery_fridge_monster_card_long_title.png")) {
+            outputStream.write(cardWithLongTitleBytes);
+        }
+
+        byte[] cardWithVeryLongTitleBytes = cardService.createCard(cardWithVeryLongTitle);
+        try (FileOutputStream outputStream = new FileOutputStream("fiery_fridge_monster_card_very_long_title.png")) {
+            outputStream.write(cardWithVeryLongTitleBytes);
+        }
+
+        //assertArrayEquals(imageWithShortTitle.getContentAsByteArray(), cardWithShortTitleBytes);
+    }
+
+    @Test
+    public void testRemoveNewLines() {
+        String inputString1 = "This is a text\nwith newlines\n\nand spaces.";
+        String expectedOutput1 = "This is a text with newlines and spaces.";
+        assertEquals(expectedOutput1, cardService.removeNewLines(inputString1));
+
+        String inputString2 = "Single newline\nin this text.";
+        String expectedOutput2 = "Single newline in this text.";
+        assertEquals(expectedOutput2, cardService.removeNewLines(inputString2));
+
+        String inputString3 = "No newlines in this text.";
+        String expectedOutput3 = "No newlines in this text.";
+        assertEquals(expectedOutput3, cardService.removeNewLines(inputString3));
+
+        String inputString5 = "\n   Text with leading and trailing spaces.   \n";
+        String expectedOutput5 = "Text with leading and trailing spaces.";
+        assertEquals(expectedOutput5, cardService.removeNewLines(inputString5));
     }
 
 }
