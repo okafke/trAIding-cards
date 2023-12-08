e905c0533116015c717d153cfd1c4d53d183c228
okafke <65917827+okafke@users.noreply.github.com>
2023-12-08 15:49:17 +0100

    Fixed CI, updated README with cost per card

--- src/main/java/io/github/okafke/aitcg/openai/OpenAIRestTemplateConfig.java
+++ src/main/java/io/github/okafke/aitcg/openai/OpenAIRestTemplateConfig.java
@@ -6,9 +6,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+
 @Configuration
 public class OpenAIRestTemplateConfig {
-    @Value("${openai.api.key}")
+    @Value("${openai.api.key:#{'No API key provided'}}")
     private String openaiApiKey;
 
     @Bean
@@ -16,6 +18,10 @@ public class OpenAIRestTemplateConfig {
     public RestTemplate openaiRestTemplate() {
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.getInterceptors().add((request, body, execution) -> {
+            if ("No API key provided".equals(openaiApiKey)) {
+                throw new IOException("No OpenAI API key has been provided");
+            }
+
             request.getHeaders().add("Authorization", "Bearer " + openaiApiKey);
             return execution.execute(request, body);
         });
